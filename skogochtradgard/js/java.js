de90472a54a458c2c658e9963ce7b65c9fca8c37
Gagamamma <sast0218@gmail.com>
2020-08-16 21:27:50 +0200

    asd

--- skogochtradgard/js/java.js
+++ skogochtradgard/js/java.js
@@ -10,8 +10,8 @@ $("#hemButton").click(function(){
   var pos=0;
   if (a<412) {
     pos=140;
-
   }
+
   else {
     pos=79;
   }
@@ -19,9 +19,6 @@ $("#hemButton").click(function(){
   $("html, body").animate({
     scrollTop: $("#hemsidan").offset().top-pos
   }, 2000);
-
-
-
 });
 
 $("#sortimentButton").click(function(){
@@ -30,21 +27,16 @@ $("#sortimentButton").click(function(){
   var pos=0;
   if (a<412) {
     pos=150;
-
   }
+
   else {
     pos=90;
   }
 
   $("html, body").animate({
-
     scrollTop: $("#sortiment").offset().top-pos
   }, 2000);
-
-
-
 });
-
 
 $("#verkstadButton").click(function(){
   var a = window.pageYOffset ;
@@ -52,19 +44,15 @@ $("#verkstadButton").click(function(){
   var pos=0;
   if (a<412) {
     pos=150;
-
   }
+
   else {
     pos=90;
   }
 
   $("html, body").animate({
-
-    scrollTop: $("#verkstad").offset().top-150
+    scrollTop: $("#verkstad").offset().top-pos
   }, 2000);
-
-
-
 });
 
 $("#oppettiderButton").click(function(){
@@ -73,19 +61,15 @@ $("#oppettiderButton").click(function(){
   var pos=0;
   if (a<412) {
     pos=150;
-
   }
+
   else {
     pos=90;
   }
 
   $("html, body").animate({
-
-    scrollTop: $("#oppettider").offset().top-150
+    scrollTop: $("#oppettider").offset().top-pos
   }, 2000);
-
-
-
 });
 
 $("#kontaktButton").click(function(){
@@ -94,41 +78,32 @@ $("#kontaktButton").click(function(){
   var pos=0;
   if (a<412) {
     pos=150;
-
   }
+
   else {
     pos=90;
   }
 
   $("html, body").animate({
-
-    scrollTop: $("#kontakt").offset().top-150
+    scrollTop: $("#kontakt").offset().top-pos
   }, 2000);
-
-
-
 });
 
 $("#omossButton").click(function(){
-
   var a = window.pageYOffset ;
   console.log(a);
   var pos=0;
   if (a<412) {
-    pos=200;
-
+    pos=188;
   }
+
   else {
-    pos=140;
+    pos=134;
   }
 
   $("html, body").animate({
-
     scrollTop: $("#omoss").offset().top-pos
   }, 2000);
-
-
-
 });
 
 $(".bild").click(function(){
@@ -137,19 +112,15 @@ $(".bild").click(function(){
   var pos=0;
   if (a<412) {
     pos=140;
-
   }
+
   else {
     pos=79;
   }
 
   $("html, body").animate({
-
     scrollTop: $("#hemsidan").offset().top-pos
   }, 2000);
-
-
-
 });
 
 window.onscroll = function() {myFunction()};
@@ -167,11 +138,6 @@ function myFunction() {
   document.body.style.zIndex = 1;
 }
 
-
-
-
-
-
 $("button").show();     //Jag säger att knappen ska synas.
 $("#slider").css("overflow", "hidden");   //Jag sätter att rutan window ska överfördet tas bort (det utanför 200*200px) när jquery är igång.
 var x=0; //initierar variabeln x
@@ -179,15 +145,12 @@ var x=0; //initierar variabeln x
 $("#btn1").click(function () {  //När man klickar på knappen btn1 körs funktionen click.
   x=x+1; //För varje gång funktionen körs plussas x med ett.
 
-
   if (x>=3) { //Om x är större eller =
     $("#imgs").animate({right:"0"});  // ska man bläddra tillbaka till bild ett på px 0.
     x=0; //x blir 0 igen eftersom man är på första bilden.
   }else {
     $("#imgs").animate({right:"+=303"});  //Innan x>=3 ska bilderna bläddra 202px mot höger.
-
   }
-
 });
 
 $("#btn2").click(function () { //När man klickar på knappen btn2 körs funktionen klick.
