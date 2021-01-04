956640dacef756e0fa60ced12db5d9c773aa8ba7
Gagamamma <gagamammaband@gmail.com>
2021-01-04 16:15:16 +0100

    asd

--- SamuelsTotalservice/js/motto.js
+++ SamuelsTotalservice/js/motto.js
@@ -4,5 +4,5 @@ console.log(`Letter ${i} is ${pil[i].getTotalLength()}`);
 }
 
 setTimeout(function() {
-  window.location.href = "file:///C:/Users/samstr1/github/slider/SamuelsTotalservice/index.html";
+  window.location.href = "https://gagamamma.github.io/slider/SamuelsTotalservice/index.html";
 }, 4500);
